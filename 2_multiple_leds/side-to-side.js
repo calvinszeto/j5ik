04fe17fd2192c6c77808e6d89fb705dcd8337de6
Calvin Szeto <szeto.calvin@gmail.com>
2016-12-01 02:50:28 -0500

    Get multiple LEDs blinking in a row.

---
+++ 2_multiple_leds/side-to-side.js
@@ -0,0 +1,18 @@
+var five = require("johnny-five");
+var Tessel = require("tessel-io");
+var board = new five.Board({
+  io: new Tessel()
+});
+board.on("ready", () => {
+  var leds = new five.Leds(["b0", "b1", "b2", "b3", "b4", "b5"]);
+  var index = 0;
+  var step = 1;
+  board.loop(100, () => {
+    leds.off();
+    leds[index].on();
+    index += step;
+    if (index === 0 || index === leds.length - 1) {
+      step *= -1;
+    }
+  })
+})
